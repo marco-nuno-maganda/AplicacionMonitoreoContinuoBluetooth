419e24011611dc8f2b2337b2dd64ac9e495e2812
Marco Nuño Maganda <marco.nuno.maganda@gmail.com>
2022-03-18 18:41:32 -0600

    Primer commit

---
+++ Z.sh
@@ -0,0 +1,12 @@
+NombreRepositorio=AplicacionMonitoreoContinuoBluetooth 
+Rama=main3
+rm -rf .git
+
+
+git init
+#git add PythonBasicos/*.*
+git add *.*
+git commit -m "Primer commit"
+git remote add origin https://github.com/marco-nuno-maganda/${NombreRepositorio}.git
+git branch -M ${Rama}
+git push -u origin ${Rama}
